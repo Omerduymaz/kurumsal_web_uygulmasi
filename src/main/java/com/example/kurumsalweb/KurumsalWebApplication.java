deaf2394f6bb62b5451c0a8fc24e4f674551e5e2
Omerduymaz <duymaz.o2121@gmail.com>
2024-11-01 13:30:56 +0300

    initial commit

---
+++ src/main/java/com/example/kurumsalweb/KurumsalWebApplication.java
@@ -0,0 +1,16 @@
+package com.example.kurumsalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KurumsalWebApplication {
+    
+
+    
+    public static void main(String[] args) {
+        SpringApplication.run(KurumsalWebApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/kurumsalweb/controller/ContactController.java
@@ -0,0 +1,56 @@
+package com.example.kurumsalweb.controller;
+
+
+import com.example.kurumsalweb.model.ContactModel;
+import com.example.kurumsalweb.repository.ContactRepository;
+import com.example.kurumsalweb.service.ContactService;
+import com.example.kurumsalweb.service.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class ContactController {
+    @Autowired
+    private ContactRepository contactRepository;
+    
+    @Autowired
+    private ContactService contactService;
+    
+    @Autowired
+    private EmailSenderService senderService;
+    
+    
+    @PostMapping("/submitForm")
+    public String submitForm(@RequestParam("ad") String ad, @RequestParam("mail") String mail,@RequestParam("telefon") String telefon) {
+        ContactModel contact =new ContactModel();
+        contact.setAd(ad);
+        contact.setMail(mail);
+        contact.setTelefon(telefon);
+        contactRepository.save(contact);
+        
+        // E-posta gönderimi
+        String subject = "Yeni İletişim Formu Alındı ! ";
+        String body = "Ad: " + ad + "\nMail: " + mail + "\nTelefon: " + telefon;
+        senderService.sendMail("yourmail@gmail.com", subject, body);
+        
+        
+        return "redirect:/success.html";
+    }
+    
+    
+    @GetMapping("/admin/contacts")
+    @ResponseBody
+    public List<ContactModel> getAllContacts() {
+        return contactService.getAllContacts();
+    }
+    
+    @DeleteMapping("admin/contacts/{id}")
+    public void deleteContact(@PathVariable Long id) {
+        contactService.deleteContactById(id);
+    }
+}
